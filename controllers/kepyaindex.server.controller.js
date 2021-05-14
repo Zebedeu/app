b7b7dfb38c40a1f03fe9f824b228f71a6fe88c4d
Zebedeu <marciozebedeu@gmail.com>
2021-05-14 08:38:17 +0100

    update

--- controllers/kepyaindex.server.controller.js
+++ controllers/kepyaindex.server.controller.js
@@ -36,8 +36,8 @@ exports.kepyaIndex = function (req, res) {
 		},
 		labels,
 		moment,
-		breadcrumb: "<li class='breadcrumb-item'><a href='" + config.base_url + req.session.user_type+"/dashboard'>" + labels['LBL_HOME'][(req.session.language || config.default_language_code)] + "</a></li><li class='breadcrumb-item active' aria-current='page'>" + labels['LBL_KEPYA_INDEX'][(req.session.language || config.default_language_code)] + "</li>",
-		language: req.session.language || config.default_language_code,
+		breadcrumb: "<li class='breadcrumb-item'><a href='" + config.base_url + req.session.user_type+"/dashboard'>" + labels['LBL_HOME'][(req.session.language || 'PT')] + "</a></li><li class='breadcrumb-item active' aria-current='page'>" + labels['LBL_KEPYA_INDEX'][(req.session.language || 'PT')] + "</li>",
+		language: req.session.language || 'PT',
 		messages: req.flash('error') || req.flash('info'),
 		messages: req.flash('info')
 	});
