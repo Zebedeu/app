a4cad10a6e42e33453948b9596043e53119e03f3
Zebedeu <marciozebedeu@gmail.com>
2021-05-14 10:10:11 +0100

    update

--- routes/kepyaindex.server.routes.js
+++ routes/kepyaindex.server.routes.js
@@ -1,11 +1,11 @@
-let index = require('../controllers/kepyaindex.server.controller.js');
+let index_k = require('../controllers/kepyaindex.server.controller.js');
 const {
     authenticate_common
 } = require('../utils/session.js');
 
 module.exports = function(app) {
-	app.get('/kepyaindex', authenticate_common, index.kepyaIndex);
-	app.get('/get-kepya-index/:dateTo', authenticate_common, index.getKepyaIndex);
+	app.get('/kepyaindex', authenticate_common, index_k.kepyaIndex);
+	app.get('/get-kepya-index/:dateTo', authenticate_common, index_k.getKepyaIndex);
 
 
 };
